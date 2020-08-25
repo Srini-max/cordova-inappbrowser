2906dd5d50481ca4e435295e3c9b8abc106c367e
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-08-25 13:28:33 +0400

    Update CDVUIInAppBrowser.m

--- src/ios/CDVUIInAppBrowser.m
+++ src/ios/CDVUIInAppBrowser.m
@@ -154,9 +154,9 @@ static CDVUIInAppBrowser* instance = nil;
     }
 
     if (self.inAppBrowserViewController == nil) {
-        NSString* userAgent = [CDVUserAgentUtil originalUserAgent];
-        NSString* overrideUserAgent = [self settingForKey:@"OverrideUserAgent"];
-        NSString* appendUserAgent = [self settingForKey:@"AppendUserAgent"];
+        NSString* userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1";// [CDVUserAgentUtil originalUserAgent];
+        NSString* overrideUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1";// [self settingForKey:@"OverrideUserAgent"];
+        NSString* appendUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1";//[self settingForKey:@"AppendUserAgent"];
         if(overrideUserAgent){
             userAgent = overrideUserAgent;
         }
